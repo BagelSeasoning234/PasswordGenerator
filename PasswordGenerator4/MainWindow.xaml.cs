69a9a0318c55252de2bde1d18011a1165b59333c
Christopher-C-1 <70250943+Christopher-C-1@users.noreply.github.com>
2022-07-09 16:05:36 -0700

    Add project files.

---
+++ PasswordGenerator4/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordGenerator4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        
+        PasswordGenerator PasswordGen;
+
+        EPasswordType TypeOfPasswordToGen = EPasswordType.All;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PasswordGen = new PasswordGenerator();
+        }
+
+        private void Generate12Chars(object sender, RoutedEventArgs e)
+        {
+            initializeGeneration(12);
+        }
+
+        private void Generate16Chars(object sender, RoutedEventArgs e)
+        {
+            initializeGeneration(16);
+        }
+
+        private void Generate20Chars(object sender, RoutedEventArgs e)
+        {
+            initializeGeneration(20);
+        }
+
+        /// <summary>
+        /// Initializes the password generation process.
+        /// </summary>
+        /// <param name="length">The character length of the password to generate.</param>
+        private void initializeGeneration(int length)
+        {
+            PasswordField.Text = PasswordGen.GeneratePassword(length, TypeOfPasswordToGen);
+            PasswordLength.Text = length.ToString();
+        }
+
+
+        /// <summary>
+        /// Generates a password with a user specified length.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GenerateTargetLength(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordLength.Text))
+                return;
+
+            int length;
+
+            // Makes sure we're dealing with a valid number, 
+
+            if (!int.TryParse(PasswordLength.Text, out length))
+                return;
+
+            if (length == 0)
+                return;
+
+            if (length < 0)
+                return;
+
+            if (length > 32)
+            {
+                length = 32;
+                PasswordLength.Text = length.ToString();
+            }
+
+            PasswordField.Text = PasswordGen.GeneratePassword(length, TypeOfPasswordToGen);
+        }
+
+        /// <summary>
+        /// Updates the password type.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UpdatePasswordType(object sender, SelectionChangedEventArgs e)
+        {
+            switch (PasswordTypeBox.SelectedIndex)
+            {
+                case 0:
+                    TypeOfPasswordToGen = EPasswordType.Lowercase;
+                    return;
+                case 1:
+                    TypeOfPasswordToGen = EPasswordType.Uppercase;
+                    return;
+                case 2:
+                    TypeOfPasswordToGen = EPasswordType.Numbers;
+                    return;
+                case 3:
+                    TypeOfPasswordToGen = EPasswordType.AvoidAmbiguous;
+                    return;
+                case 4:
+                    TypeOfPasswordToGen = EPasswordType.AvoidSymbols;
+                    return;
+                case 5:
+                    TypeOfPasswordToGen = EPasswordType.AvoidAmbiguousAndSymbols;
+                    return;
+                case 6:
+                    TypeOfPasswordToGen = EPasswordType.All;
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// Copies the given password to the clipboard.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CopyPasswordToClipboard(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PasswordField.Text))
+                Clipboard.SetText(PasswordField.Text);
+        }
+
+        // Make sure that only numbers are added to the password length field.
+
+        private void PreviewLengthUpdated(object sender, TextCompositionEventArgs e)
+        {
+            CheckIsNumeric(e);
+            
+        }
+
+        private void CheckIsNumeric(TextCompositionEventArgs e)
+        {
+            int result;
+
+            if (!int.TryParse(e.Text, out result))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void PreviewLengthKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
+        }
+    }
+}
---
+++ PasswordGenerator4/PasswordGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator4
+{
+    enum EPasswordType
+    {
+        Lowercase,
+        Uppercase,
+        Numbers,
+        AvoidAmbiguous,
+        AvoidSymbols,
+        AvoidAmbiguousAndSymbols,
+        All
+    }
+
+    class PasswordGenerator
+    {
+
+        /// <summary>
+        /// Generates a password with the given length
+        /// </summary>
+        /// <param name="length">The number of characters</param>
+        /// <param name="passwordType">The type of password to generate</param>
+        /// <returns></returns>
+        public string GeneratePassword (int length, EPasswordType passwordType)
+        {
+
+            // Makes sure the given arguments are valid.
+
+            if (length < 0 || length == 0)
+                throw new ArgumentException("Password length must be valid.", "length");
+
+            if (length > int.MaxValue / 8)
+                throw new ArgumentException("Password length exceeds maximum value.", "length");
+
+            var characterArray = GetCharacterSet(passwordType).Distinct().ToArray();
+
+            if (characterArray.Length == 0)
+                throw new ArgumentException("Character set must not be empty.", "passwordType");
+
+            // Creates a byte array of the desired length, and fills it with random values.
+
+            byte[] bytes = new byte[length * 8];
+            new RNGCryptoServiceProvider().GetBytes(bytes);
+
+            // Fills a char array with random values from the target character set using the bytes generated earlier.
+
+            char[] result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                ulong value = BitConverter.ToUInt64(bytes, i * 8);
+                result[i] = characterArray[value % (uint)characterArray.Length];
+            }
+
+            return new string(result);
+        }
+
+        /// <summary>
+        /// Returns the character set to use, depending on the type of password chosen.
+        /// </summary>
+        /// <param name="passwordType"></param>
+        /// <returns></returns>
+        private string GetCharacterSet(EPasswordType passwordType)
+        {
+
+            const string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+            const string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string Numbers = "1234567890";
+            const string NoAmbiguous = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!@#$%^&*-_=+><";
+            const string NoSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            const string NoAmbiguousOrSymbols = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789";
+            const string AllCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+][}{/.,><;:'`~";
+
+            switch (passwordType)
+            {
+                case EPasswordType.Lowercase:
+                    return Lowercase;
+
+                case EPasswordType.Uppercase:
+                    return Uppercase;
+
+                case EPasswordType.Numbers:
+                    return Numbers;
+
+                case EPasswordType.AvoidAmbiguous:
+                    return NoAmbiguous;
+
+                case EPasswordType.AvoidSymbols:
+                    return NoSymbols;
+
+                case EPasswordType.AvoidAmbiguousAndSymbols:
+                    return NoAmbiguousOrSymbols;
+
+                case EPasswordType.All:
+                    return AllCharacters;
+
+                default:
+                    return string.Empty;
+
+            }
+        }
+    }
+}
